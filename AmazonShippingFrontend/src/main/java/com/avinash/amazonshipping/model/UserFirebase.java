9fc09feabd8e8e3cc9b58ee87ed4db101d3e2b49
Avinash Tingre <tingre.avinash@gmail.com>
2020-08-22 23:28:35 +0530

    Minor correction

---
+++ AmazonShippingFrontend/src/main/java/com/avinash/amazonshipping/model/UserFirebase.java
@@ -0,0 +1,60 @@
+package com.avinash.amazonshipping.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class UserFirebase {
+	private String username;
+	private String password;
+	private boolean enabled;
+	private boolean accountNonExpired;
+	private boolean credentialsNonExpired;
+	private boolean accountNonLocked;
+	private List<String> authorities;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public boolean isAccountNonExpired() {
+		return accountNonExpired;
+	}
+	public void setAccountNonExpired(boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+	public boolean isCredentialsNonExpired() {
+		return credentialsNonExpired;
+	}
+	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+	public boolean isAccountNonLocked() {
+		return accountNonLocked;
+	}
+	public void setAccountNonLocked(boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
+	}
+	public List<String> getAuthorities() {
+		return authorities;
+	}
+	public void setAuthorities(List<String> authorities) {
+		this.authorities = authorities;
+	}
+	
+	
+}
--- AmazonShippingFrontend/src/main/java/com/avinash/amazonshipping/service/UserDao.java
+++ AmazonShippingFrontend/src/main/java/com/avinash/amazonshipping/service/UserDao.java
@@ -1,5 +1,10 @@
 package com.avinash.amazonshipping.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,37 +14,80 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import com.avinash.amazonshipping.model.User;
+import com.avinash.amazonshipping.model.UserFirebase;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Repository
 public class UserDao {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
-	public User findUserByUsername(String username) {
-		System.out.println("findUserByUsername: "+username);
-		if("avinash".equals(username)) {
 
+	public User findUserByUsername(String username) {
+		System.out.println("findUserByUsername: " + username);
+		if ("avinash".equals(username)) {
+			UserFirebase userFirebase = new UserFirebase();
 			User user = new User();
-			/**
-			 * Call rest endpoint to get userdetails 
-			 * from firebase and map to user object
-			 */
-			user.setUsername("avinash");
-			user.setPassword(passwordEncoder.encode("1234"));
-			user.setAccountNonExpired(true);
-			user.setAccountNonLocked(true);
-			user.setCredentialsNonExpired(true);
-			user.setEnabled(true);
-			
-			SimpleGrantedAuthority authority_USER = new SimpleGrantedAuthority("USER");
-			SimpleGrantedAuthority authority_ADMIN = new SimpleGrantedAuthority("ADMIN");
-			
-			List<SimpleGrantedAuthority> updatedAuthorities = new ArrayList<SimpleGrantedAuthority>();
-			updatedAuthorities.add(authority_USER);
-			//updatedAuthorities.add(authority_ADMIN);
-			user.setAuthorities(updatedAuthorities);
-			
+			ObjectMapper objectMapper = new ObjectMapper();
+			HttpURLConnection con = null;
+			try {
+
+				URL url = new URL("http://localhost:7777/ShippingApplication/getUserDetails?username=" + username);
+				con = (HttpURLConnection) url.openConnection();
+				con.setRequestMethod("GET");
+				con.setRequestProperty("Content-Type", "application/json");
+
+				con.setConnectTimeout(5000);
+				con.setReadTimeout(5000);
+
+				int status = con.getResponseCode();
+				if (status > 299) {
+					Reader streamReader = null;
+
+					BufferedReader in = new BufferedReader(streamReader);
+					String inputLine;
+					StringBuffer content = new StringBuffer();
+					while ((inputLine = in.readLine()) != null) {
+						content.append(inputLine);
+					}
+					in.close();
+					System.out.println("Below error occured: \n" + content.toString());
+
+				} else {
+					BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					String inputLine;
+					StringBuffer content = new StringBuffer();
+					while ((inputLine = in.readLine()) != null) {
+						content.append(inputLine);
+					}
+					in.close();
+					String response = content.toString();
+					if (response != null) {
+						userFirebase = objectMapper.readValue(response, UserFirebase.class);
+						user.setAccountNonExpired(userFirebase.isAccountNonExpired());
+						user.setAccountNonLocked(userFirebase.isAccountNonLocked());
+						user.setCredentialsNonExpired(userFirebase.isCredentialsNonExpired());
+						user.setEnabled(userFirebase.isEnabled());
+						user.setPassword(passwordEncoder.encode(userFirebase.getPassword()));
+						user.setUsername(userFirebase.getUsername());
+
+						List<SimpleGrantedAuthority> updatedAuthorities = new ArrayList<SimpleGrantedAuthority>();
+						
+						for(String role: userFirebase.getAuthorities()) {
+							updatedAuthorities.add(new SimpleGrantedAuthority(role));							
+						}
+						user.setAuthorities(updatedAuthorities);
+
+					} else {
+						userFirebase = null;
+						user =null;
+					}
+				}
+			} catch (Exception ex) {
+				System.out.println("Exception caught:\n");
+				ex.printStackTrace();
+			}
+
 			return user;
 		}
 		return null;
